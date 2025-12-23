67a9f275bced4540aaaa32372d9875c9025b71a4
gintaras68 <gintaras5783@yahoo.com>
2025-12-23 12:39:02 +0200

    Added simple socumentation for JS. Remove some comments.

--- src/js/components/calculator.js
+++ src/js/components/calculator.js
@@ -1,3 +1,20 @@
+/**
+ *  Sukuriamas kalkuliatoriaus komponentas duoto DOM-elemento viduje.
+ * @param {HTMLElement|string} root
+ *  DOM-elementas, kuriame randasi kalkuliatoriaus sužymėjimas
+ *  arba CSS-selektoriaus, kuris žymi įėjimą į jį, eilutė.
+ *
+ * @returns {void}
+ *
+ * @example
+ * // Vienas kalkuliatorius (labiausiai tikėtinas atvejis)
+ * const el = document.querySelector(".calc");
+ * createCalculator(el);
+ *
+ * @example
+ * // Keli kalkuliatoriai
+ * document.querySelectorAll(".calc").forEach(createCalculator);
+ */
 export function createCalculator(root) {
   let device = null;
 
@@ -12,12 +29,12 @@ export function createCalculator(root) {
     return;
   }
 
-  const DISPLAY_LENGTH = 17; // maksimaklus simboliu kiekis ekrane
+  const DISPLAY_LENGTH = 17;
   const buttons = device.querySelectorAll(".keyboard__button");
   const screen = device.querySelector(".calc__screen");
 
-  let isNew = true; // naujo skaitmens indikatorius (nuimamas po pirmo skaitmens simbolio ivedimo)
-  let numberString = "0"; // bus renkamas skaitmuo teksto formoje
+  let isNew = true;
+  let numberString = "0";
   let operand = "";
   let isArg1 = false;
   let isArg2 = false;
@@ -25,29 +42,6 @@ export function createCalculator(root) {
   let arg1,
     arg2,
     rezult = null;
-
-  showVariables("PRADINE BUSENA");
-
-  /* ---------      Darbo logika    -------------------------
-  Mygtukas      numberString  screenC.. |  arg1  arg2  rezult  operand  | paaiskinimas
-  - pradine b.    0           0             X     X     X                 pradioj tuscia, ekrane nulis
-  -----------------------------------------------------------------------------------------------------
-  operacija       0           0             X     X     X                 negalimas veiksmas !!
-  kablelis .      0 -> 0.     0 -> 0.       X     X     X                 pries kableli - nulis
-  skaicius 7      0 -> 7      0 -> 7        X     X     X                 sveika dalis  - ne nulis
-  kablelis .    7   -> 7.     7 -> 7.       X     X     X                 skaicius gauna trupmenos zenkla
-  skaicius 2    7.  -> 7.2    7. -> 7.2     X     X     X                 skaiciaus eilute papildoma
-  kablelis .    7.2 -> 7.2    7.2 -> 7.2    X     X     X                 antro kablelio negali buti !!
-  -----------------------------------------------------------------------------------------------------
-    C             ->0          ->0        ->X    ->X   ->X                viskas i pradzia ???
-
-  operand  +     Number()    7.2 -> 7.2+  X->7.2  X     X       +     Nera pirmo OP -> suteik. reikme
-                                                                      ir ekrane - operando simbolis
-  operand  -        +        7.2+ -> 7.2-   7.2   X     X       -     Stebim operanda - galim pakeisti
-  operand  -        -        7.2- -> 7.2-   7.2   X     X       -     Stebim operanda - galim pakeisti
-
-
-*/
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -63,7 +57,6 @@ export function createCalculator(root) {
         }
         loadNum(btnRole);
         renderScreen();
-        showVariables("51 - formuojamas skaicius", btnRole);
         return;
       }
 
@@ -71,7 +64,6 @@ export function createCalculator(root) {
       if (btnRole === "C") {
         clearAll();
         renderScreen();
-        showVariables("59 - Istrinu viska", btnRole);
         return;
       }
 
@@ -80,10 +72,7 @@ export function createCalculator(root) {
       // ------------------
 
       // <--- ignoruojam "=" jei dar nebuvo ivestas veiksmas
-      if (btnRole === "=" && !operand) {
-        console.log("nera nurodyta operacija", btnRole);
-        return;
-      }
+      if (btnRole === "=" && !operand) return;
 
       // -----  surinktas 1-as argumentas ir operandas (ne ligybe!):
       if (!isArg1 && btnRole !== "=") {
@@ -92,12 +81,10 @@ export function createCalculator(root) {
         isArg1 = isNew = true;
         operand = btnRole;
         renderScreen();
-        showVariables("83 - Pirmas skaitmuo ivestas", btnRole);
         return;
       }
 
       if (isArg1 && !isArg2) {
-        console.log("-----   Veiksmai kai nera antro argumento ...   -------");
         if (numberString === "") {
           operand = btnRole;
           renderScreen();
@@ -138,7 +125,7 @@ export function createCalculator(root) {
           break;
       }
       isResult = true;
-      // dirbam su rezultatu
+
       if (btnRole === "=") {
         renderScreen();
       } else {
@@ -149,30 +136,12 @@ export function createCalculator(root) {
         renderScreen();
         isNew = true;
       }
-      showVariables("Po atliktu skaicaivimu", btnRole);
     });
   });
 
   // ----- additional functions...  -----------------------
-  function showVariables(comment = "", role = "?") {
-    const l1 = isArg1 ? "Yes" : "No ";
-    const l2 = isArg2 ? "Yes" : "No ";
-    const l3 = isResult ? "Yes" : "No ";
-    const l4 = isNew ? "Yes" : "No ";
-    console.log(
-      `↓¯¯¯¯ isArg1 | operand | isArg2 | isResult | isNew | <<< btn > numbStr. >> arg1 / arg2 / rezult | ¯¯¯  ${comment} ¯¯↓`
-    );
-    console.log(
-      ` →      ${l1}     > ${operand} <      ${l2}       ${l3}      ${l4}   | <<< "${role}" >  "${numberString}" >>  ${arg1}  /  ${arg2}  /  ${rezult} |`
-    );
-    console.log("");
-  }
-
   function loadNum(symb) {
-    // nereaguojame jei bandoma iterpti antra kableli
-    if (symb === "." && numberString.includes(".")) {
-      return;
-    }
+    if (symb === "." && numberString.includes(".")) return;
 
     if (isNew) {
       if (symb === ".") {
@@ -217,9 +186,8 @@ export function createCalculator(root) {
   }
 
   function formatResult(value, maxLen = DISPLAY_LENGTH - 4) {
-    if (value === null || value === undefined) return ""; // nereraguojam jei nera rezultato
-    if (!Number.isFinite(value)) return "Error"; // klaidos pranesimas jei rezultatas - begalinis skaicius
-
+    if (value === null || value === undefined) return "";
+    if (!Number.isFinite(value)) return "Error";
     let text = value.toString();
     if (text.length <= maxLen) return text;
 
@@ -233,7 +201,20 @@ export function createCalculator(root) {
     // jei yra, pasalinami nereiklaingi numeriai
     text = text.replace(/\.?0+e/, "e").replace(/\.?0+$/, "");
 
-    console.log("Rezultatas: ", value, " |> Suformatavus: ", text);
     return text;
+  }
+
+  function showVariables(comment = "", role = "?") {
+    const l1 = isArg1 ? "Yes" : "No ";
+    const l2 = isArg2 ? "Yes" : "No ";
+    const l3 = isResult ? "Yes" : "No ";
+    const l4 = isNew ? "Yes" : "No ";
+    console.log(
+      `↓¯¯¯¯ isArg1 | operand | isArg2 | isResult | isNew | <<< btn > numbStr. >> arg1 / arg2 / rezult | ¯¯¯  ${comment} ¯¯↓`
+    );
+    console.log(
+      ` →      ${l1}     > ${operand} <      ${l2}       ${l3}      ${l4}   | <<< "${role}" >  "${numberString}" >>  ${arg1}  /  ${arg2}  /  ${rezult} |`
+    );
+    console.log("");
   }
 }
